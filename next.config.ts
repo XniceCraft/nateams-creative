1d459fec22303ddda5d1ff18c34bc74eaa8909b9
XniceCraft <xnicecraftacc@gmail.com>
2026-06-07 18:33:58 +0700

    feat: add base path option

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   typedRoutes: true,
+  basePath: process.env.NEXT_PUBLIC_BASE_URL,
 };
 
 export default nextConfig;
